f80ad82b16f4c831e46d755ccc9bf47efa250383
dhivyamurugesan20 <171238022+dhivyamurugesan20@users.noreply.github.com>
2026-01-25 13:39:59 +0530

    Add files via upload
    solved c program"Print the difference of two integers using -."

---
+++ 30program.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main() {
+    int a, b, diff;
+    a = 8; b = 3;
+    diff = a - b;
+    printf("%d\n", diff);  
+    a = 10; b = 5;
+    diff = a - b;
+    printf("%d\n", diff);  
+    a = 15; b = 10;
+    diff = a - b;
+    printf("%d\n", diff); 
+    return 0;
+}
