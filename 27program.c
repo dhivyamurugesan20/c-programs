12c6089cb38a34efb1fd31080556f103cd5270be
dhivyamurugesan20 <171238022+dhivyamurugesan20@users.noreply.github.com>
2026-01-25 13:26:43 +0530

    Add files via upload
    solved c program"Assign int to float"

---
+++ 27program.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main() {
+    int a = 10;     
+    float f1 = a;     
+    printf("%.1f\n", f1);
+    int b = 25;       
+    float f2 = b;    
+    printf("%.1f", f2);
+    return 0;
+}
