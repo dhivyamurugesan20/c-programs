aafcb00d3a94359a367a342aac0f50a0e04d158f
dhivyamurugesan20 <171238022+dhivyamurugesan20@users.noreply.github.com>
2026-01-25 13:31:05 +0530

    Add files via upload
    solved c program"Store char in int"

---
+++ 28program.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main() {
+    char x = 'A';   
+    int num1 = x;   
+    printf("%d\n", num1);  
+    char y = 'a';   
+    int num2 = y;   
+    printf("%d\n", num2);  
+    return 0;
+}
