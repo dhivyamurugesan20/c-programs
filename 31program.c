49d595eb58c54b4f9812ae7e3ef13867513d2572
dhivyamurugesan20 <171238022+dhivyamurugesan20@users.noreply.github.com>
2026-01-25 13:43:31 +0530

    Add files via upload
    solved c program"Print the product of two integers using *."

---
+++ 31program.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main() {
+    int a, b, product;
+    a = 4; b = 5;
+    product = a * b;
+    printf("%d\n", product);
+    a = 10; b = 2;
+    product = a * b;
+    printf("%d\n", product);
+    a = 1; b = 20;
+    product = a * b;
+    printf("%d\n", product);
+    return 0;
+}
