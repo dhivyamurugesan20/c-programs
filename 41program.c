9b7fec0775fcf35ced3bd5493ef8de2d1d823a6f
dhivyamurugesan20 <171238022+dhivyamurugesan20@users.noreply.github.com>
2026-01-25 17:25:40 +0530

    Update 41program.c

--- 41program.c
+++ 41program.c
@@ -1,7 +1,7 @@
-#include<stdio.h>
-int main(){
-    int a,b,c;
-    a=5,b=5,c=(a==b);
-    printf("%d",c);
+#include <stdio.h>
+int main() {
+    int a, b;  
+    scanf("%d %d", &a, &b);   
+    printf("%d\n", a==b); 
     return 0;
 }
