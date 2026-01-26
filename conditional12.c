59b292c1599a371779e3fb90f684220ddfab17da
dhivyamurugesan20 <171238022+dhivyamurugesan20@users.noreply.github.com>
2026-01-26 21:46:09 +0530

    Add files via upload
    solved c program"Grade Calculation".

---
+++ conditional12.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main() {
+   int a;
+   scanf("%d",&a);
+   printf("%d",a);
+   if (a >= 90) {
+        printf("Grade A");
+    } else if (a >=75) {
+        printf("Grade B");
+    } else {
+        printf("Grade C");
+    }
+    return 0;
+}
