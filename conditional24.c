58d7801c262568d36024f67dbcf880815ba1a9fe
dhivyamurugesan20 <171238022+dhivyamurugesan20@users.noreply.github.com>
2026-01-26 22:26:09 +0530

    Add files via upload
    solved  c program"Vowel Using Switch"

---
+++ conditional24.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int main() {
+    char ch;
+    printf("Enter a character: ");
+    scanf(" %c", &ch);  
+    switch(ch) {
+        case 'a':
+        case 'A':
+        case 'e':
+        case 'E':
+        case 'i':
+        case 'I':
+        case 'o':
+        case 'O':
+        case 'u':
+        case 'U':
+            printf("Vowel");
+            break;
+        default:
+            printf("Not a vowel");
+    }
+    return 0;
+}
