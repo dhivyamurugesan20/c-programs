5ce4db6afacc0146ffa8a131c66eb4645a92a66e
dhivyamurugesan20 <171238022+dhivyamurugesan20@users.noreply.github.com>
2026-01-26 20:01:33 +0530

    Add files via upload
    solved c program"Check Even or Odd"

---
+++ conditional2.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(){
+    int m;
+    scanf("%d",&m);
+    if(m%2==0){
+        printf("%d is Even Number");
+    }else{
+        printf("%d is Odd Number");
+    }
+    return 0;
+}
