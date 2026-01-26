460eeb74a36a6d562a2b96b3a51a96747afbfa49
dhivyamurugesan20 <171238022+dhivyamurugesan20@users.noreply.github.com>
2026-01-26 22:24:28 +0530

    Add files via upload
    solved c program"Simple Calculator (Switch)"

---
+++ conditional21.c
@@ -0,0 +1,22 @@
+int main() {
+    int a, b;
+    char op;
+    scanf("%d %c %d", &a, &op, &b);
+    switch (op) {
+        case '+':
+            printf("Result = %d", a + b);
+            break;
+        case '-':
+            printf("Result = %d", a - b);
+            break;
+        case '*':
+            printf("Result = %d", a * b);
+            break;
+        case '/':
+            printf("Result = %d", a / b);
+            break;
+        default:
+            printf("Invalid operator");
+    }
+    return 0;
+}
