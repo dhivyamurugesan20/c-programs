c69211de160a373361df72c7f0049fee877b53cb
dhivyamurugesan20 <171238022+dhivyamurugesan20@users.noreply.github.com>
2026-01-28 07:25:12 +0530

    Add files via upload
    solved c program"Bank Interest Calculation".

---
+++ 7switch.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main() {
+    int accountType, years;
+    scanf("%d", &accountType);
+    scanf("%d", &years);
+    switch(accountType) {
+        case 1:
+            printf("Interest 4%%");
+            break;
+        case 2:
+            if(years <= 3) {
+                printf("Interest 5%%");
+            } else {
+                printf("Interest 7%%");
+            }
+            break;
+        default:
+            printf("Invalid account type\n");
+    }
+    return 0;
+}
