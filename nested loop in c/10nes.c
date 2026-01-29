84f319ed9f52f322dd1328be2591b1f3191c3557
dhivyamurugesan20 <171238022+dhivyamurugesan20@users.noreply.github.com>
2026-01-29 22:55:09 +0530

    Add files via upload
    solved nested loop in c.

---
+++ nested loop in c/10nes.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main (){
+    for (int i=0;i<5;i++){
+        for (int j=0;j<i+1;j++){
+            printf("*");
+        }
+        printf("\n");
+    }
+    for (int i=4;i>0;i--){
+        for (int j=1;j<=i;j++){
+            printf("*");
+        }
+        printf("\n");
+    }
+    return 0;
+}
